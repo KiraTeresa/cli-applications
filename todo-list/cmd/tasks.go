185aaa19bfefe7ae6ef312ec5f4a29cb70a803a3
KiraTeresa <kira.soba@gmx.de>
2026-01-28 09:37:46 +0100

    todo-list: implement Complete()

--- todo-list/cmd/tasks.go
+++ todo-list/cmd/tasks.go
@@ -82,7 +82,59 @@ func List() {
 }
 
 func Complete(id string) {
-	fmt.Printf("Completing task with id '%s'\n", id)
+	// open csv file
+	file, err := os.OpenFile("tasks.csv", os.O_RDWR, 0660)
+	if err != nil {
+		fmt.Println("Error opening tasks.csv:", err)
+		return
+	}
+	defer file.Close()
+
+	// read data from csv file
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println("Error reading tasks.csv:", err)
+		return
+	}
+
+	// find index of the entry which should be modified
+	idx := slices.IndexFunc(records, func(r []string) bool {
+		return len(r) > 0 && r[0] == id
+	})
+	if idx == -1 {
+		log.Printf("Task with id %s not found\n", id)
+		return
+	}
+
+	// update entry
+	records[idx][2] = "done"
+
+	// truncate & reset file before writing
+	if err := file.Truncate(0); err != nil {
+		log.Println("Error truncating file:", err)
+		return
+	}
+
+	// ensure to start writing at the beginning
+	if _, err := file.Seek(0, 0); err != nil {
+		log.Println("Error seeking file:", err)
+		return
+	}
+
+	// write updated data to csv file
+	w := csv.NewWriter(file)
+	if err := w.WriteAll(records); err != nil {
+		log.Fatalln("error updating tasks.csv:", err)
+	}
+
+	// ensure all buffered data was written to the file
+	w.Flush()
+
+	// ensure no I/O errors occurred
+	if err := w.Error(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func Delete(id string) {
