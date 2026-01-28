b8764c047b6629d498eb9def3002cc6050081ac2
KiraTeresa <kira.soba@gmx.de>
2026-01-28 16:22:00 +0100

    todo-list: implement and use Task struct

--- todo-list/cmd/tasks.go
+++ todo-list/cmd/tasks.go
@@ -10,6 +10,12 @@ import (
 	"text/tabwriter"
 )
 
+type Task struct {
+	id     string
+	title  string
+	status string
+}
+
 func Add(task string) {
 	// open csv readFile if available, otherwise create it
 	readFile, err := os.OpenFile("tasks.csv", os.O_RDONLY|os.O_CREATE, 0660)
@@ -19,7 +25,7 @@ func Add(task string) {
 	}
 
 	// read the data from the csv readFile
-	records := getFileRecords(readFile)
+	tasks := getTasksFromFile(readFile)
 	readFile.Close()
 
 	// open csv file with append flag
@@ -34,19 +40,19 @@ func Add(task string) {
 	w := csv.NewWriter(appendFile)
 
 	// write a header if the readFile was newly created
-	if len(records) == 0 {
-		header := []string{"id", "task", "status"}
-		err := w.Write(header)
+	if len(tasks) == 0 {
+		header := Task{"id", "title", "status"}
+		err := w.Write(taskToRecord(header))
 		if err != nil {
-			log.Fatalln("error writing header to task.csv:", err)
+			log.Fatalln("error writing header to Task.csv:", err)
 		}
 	}
 
 	// if file is empty or only contains header, start with id=1
-	if len(records) <= 1 {
+	if len(tasks) <= 1 {
 		nextId = "1"
 	} else { // otherwise set id to the next number
-		lastId, err := strconv.Atoi(records[len(records)-1][0])
+		lastId, err := strconv.Atoi(tasks[len(tasks)-1].id)
 		if err != nil {
 			fmt.Println("error evaluating last id:", err)
 			return
@@ -55,8 +61,8 @@ func Add(task string) {
 	}
 
 	// create new data & write it to the readFile
-	s := []string{nextId, task, "open"}
-	if err := w.Write(s); err != nil {
+	s := Task{nextId, task, "open"}
+	if err := w.Write(taskToRecord(s)); err != nil {
 		log.Fatalln("error writing to tasks.csv:", err)
 	}
 
@@ -79,19 +85,19 @@ func List(listAll bool) {
 	defer file.Close()
 
 	// retrieve all data from the csv file
-	records := getFileRecords(file)
+	tasks := getTasksFromFile(file)
 
-	var result [][]string
+	var result []Task
 	if listAll {
 		// list all tasks
-		result = records
+		result = tasks
 	} else {
 		// only list tasks with status "open"
-		result = make([][]string, 0, len(records))
+		result = make([]Task, 0, len(tasks))
 
-		for i, r := range records {
-			if i == 0 || r[2] == "open" {
-				result = append(result, r)
+		for i, t := range tasks {
+			if i == 0 || t.status == "open" {
+				result = append(result, t)
 			}
 		}
 	}
@@ -100,7 +106,7 @@ func List(listAll bool) {
 	tw.Init(os.Stdout, 0, 8, 2, ' ', 0)
 
 	for i, r := range result {
-		fmt.Fprintf(tw, "%s\t%s\t%s\n", r[0], r[1], r[2])
+		fmt.Fprintf(tw, "%s\t%s\t%s\n", r.id, r.title, r.status)
 		if i == 0 {
 			fmt.Fprintf(tw, "--\t----\t------\n")
 		}
@@ -118,15 +124,15 @@ func Complete(id string) {
 	defer file.Close()
 
 	// read data from csv file
-	records := getFileRecords(file)
+	tasks := getTasksFromFile(file)
 
 	// find index of the entry which should be modified
-	idx := getIndexOfId(id, records)
+	idx := getIndexOfId(id, tasks)
 
 	// update entry
-	records[idx][2] = "done"
+	tasks[idx].status = "done"
 
-	updateFile(file, records)
+	updateFile(file, tasks)
 }
 
 func Delete(id string) {
@@ -139,7 +145,7 @@ func Delete(id string) {
 	defer file.Close()
 
 	// read data from csv file
-	records := getFileRecords(file)
+	records := getTasksFromFile(file)
 
 	// find index of the entry which should be deleted
 	idx := getIndexOfId(id, records)
@@ -155,7 +161,7 @@ func Delete(id string) {
 	updateFile(file, records)
 }
 
-func updateFile(file *os.File, data [][]string) {
+func updateFile(file *os.File, data []Task) {
 	// truncate & reset file before writing
 	if err := file.Truncate(0); err != nil {
 		log.Println("Error truncating file:", err)
@@ -170,7 +176,7 @@ func updateFile(file *os.File, data [][]string) {
 
 	// write updated data to csv file
 	w := csv.NewWriter(file)
-	if err := w.WriteAll(data); err != nil {
+	if err := w.WriteAll(tasksToRecords(data)); err != nil {
 		log.Fatalln("error updating tasks.csv:", err)
 	}
 
@@ -183,24 +189,49 @@ func updateFile(file *os.File, data [][]string) {
 	}
 }
 
-func getIndexOfId(id string, records [][]string) int {
+func getIndexOfId(id string, tasks []Task) int {
 	// find index of the entry which should be modified
-	idx := slices.IndexFunc(records, func(r []string) bool {
-		return len(r) > 0 && r[0] == id
+	idx := slices.IndexFunc(tasks, func(t Task) bool {
+		return t.id == id
 	})
-	if idx == -1 {
+
+	if idx <= 0 {
 		log.Printf("Task with id %s not found\n", id)
 		os.Exit(1)
 	}
 	return idx
 }
 
-func getFileRecords(file *os.File) [][]string {
+func getTasksFromFile(file *os.File) []Task {
 	reader := csv.NewReader(file)
 	records, err := reader.ReadAll()
 	if err != nil {
 		fmt.Println("Error reading tasks.csv:", err)
 		os.Exit(1)
+	}
+	return recordsToTask(records)
+}
+
+func recordsToTask(records [][]string) []Task {
+	var tasks []Task
+	for _, r := range records {
+		tasks = append(tasks, Task{
+			id:     r[0],
+			title:  r[1],
+			status: r[2],
+		})
+	}
+	return tasks
+}
+
+func taskToRecord(t Task) []string {
+	return []string{t.id, t.title, t.status}
+}
+
+func tasksToRecords(tasks []Task) [][]string {
+	var records [][]string
+	for _, t := range tasks {
+		records = append(records, taskToRecord(t))
 	}
 	return records
 }
